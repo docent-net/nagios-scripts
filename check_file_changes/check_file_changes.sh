44be7e3c49eeecb6b7196e53c9b96f7a6ca9b8a2
Maciej Lasyk <docent.net@gmail.com>
2013-10-15 17:55:29 +0200

    license fix

--- check_file_changes/check_file_changes.sh
+++ check_file_changes/check_file_changes.sh
@@ -16,6 +16,20 @@
 #	./check_file_changes.sh -f somelogfile/file_:WEEK:.txt -h 00:00:00 -r :WEEK: -z 8
 #
 ##############################################################################
+# Copyright 2013 Maciej Lasyk
+#
+#	 Licensed under the Apache License, Version 2.0 (the "License");
+#	 you may not use this file except in compliance with the License.
+#	 You may obtain a copy of the License at
+#
+#		 http://www.apache.org/licenses/LICENSE-2.0
+#
+#	 Unless required by applicable law or agreed to in writing, software
+#	 distributed under the License is distributed on an "AS IS" BASIS,
+#	 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#	 See the License for the specific language governing permissions and
+#	 limitations under the License.
+##############################################################################
 
 FIND=/usr/bin/find
 STATE_OK=0
